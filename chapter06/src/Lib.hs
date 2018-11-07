26a3ffe42034c3a12406c91a9bcf671819c32b3a
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-11-07 12:42:28 +0900

    Chapter 06

--- chapter06/src/Lib.hs
+++ chapter06/src/Lib.hs
@@ -2,7 +2,10 @@ module Lib
     ( someFunc,
     factorial, sumdown,
     pow, gcd',
-    length', init', drop'
+    length', init', drop',
+    concat', and', replicate',
+    idx, merge, msort,
+    last', take', sum'
     ) where
 
 import Prelude hiding (gcd)
@@ -42,3 +45,58 @@ init' (x:xs) = x : (init' xs)
 drop' :: Int -> [a] -> [a]
 drop' 0 xs = xs
 drop' n (x:xs) = drop' (n-1) xs
+
+and' :: [Bool] -> Bool
+and' [] = True
+and' (x:xs) = x && (and' xs)
+
+concat' :: [a] -> [a] -> [a]
+concat' xs [] = xs
+concat' xs (y:ys) = concat' (xs ++ [y]) ys
+
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n a = a : replicate' (n-1) a
+
+idx :: [a] -> Int -> a
+idx (x:xs) 0 = x
+idx (x:xs) n = idx xs (n-1)
+idx _ _ = error "Index is too large"
+
+-- Merge sort
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs ys = reverse (merge' xs ys [])
+
+merge' :: Ord a => [a] -> [a] -> [a] -> [a]
+merge' [] ys result = (reverse ys) ++ result 
+merge' xs [] result = (reverse xs) ++ result
+merge' xss@(x:xs) yss@(y:ys) result
+  | x <= y = merge' xs yss (x:result)
+  | otherwise = merge' xss ys (y:result)
+
+halve :: [a] -> ([a], [a])
+halve xs =
+  let l = length xs
+      middle = l `div` 2
+   in (take middle xs, drop middle xs)
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort (x:[]) = [x]
+msort xs =
+  let (h, t) = halve xs
+   in merge (msort h) (msort t)
+
+sum' :: [Int] -> Int
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+take' :: Int -> [a] -> [a]
+take' _ [] = []
+take' 0 _ = []
+take' n (x:xs) = x : (take' (n - 1) xs)
+
+last' :: [a] -> a
+last' [] = error "empty list"
+last' (x:[]) = x
+last' (x:xs) = last' xs
