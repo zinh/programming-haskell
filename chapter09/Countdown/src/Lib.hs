e5a3753c1e8b9ae8da658dc60f532322d0658618
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-11-13 18:25:52 +0900

    Chapter 9

--- chapter09/Countdown/src/Lib.hs
+++ chapter09/Countdown/src/Lib.hs
@@ -1,9 +1,31 @@
 module Lib
-    ( someFunc
+    ( someFunc, choices
     ) where
+
+import Expr (Expr, eval)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 interleave :: a -> [a] -> [[a]]
-interleave x xs
+interleave x [] = [[x]]
+interleave x (y:ys) = (x:y:ys) : [y:i | i <- interleave x ys]
+
+-- Permutation
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms [x] = [[x]]
+perms (x:xs) = [y | ys <- perms xs, y <- interleave x ys]
+
+-- Subset
+subs :: [a] -> [[a]]
+subs [] = [[]]
+subs (x:xs) =
+  let subsn = subs xs
+   in subsn ++ [x:xss | xss <- subsn]
+
+choices :: [a] -> [[a]]
+choices = concat . map perms . subs
+
+solution :: Expr -> [Int] -> Int -> Bool
+solution expr ns result = eval expr == result
